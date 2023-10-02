cf7a3e2e2aafee1b5610d1834abb1000569d815f
katarighe <m.ighe@hotmail.com>
2023-10-02 19:02:35 +0300

    Fix typo on require

--- classes/test.rb
+++ classes/test.rb
@@ -1,6 +1,6 @@
-require_relative './person'
-require_relative './capitalize_decorator'
-require_relative './trimmer_decorator'
+require './person'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
