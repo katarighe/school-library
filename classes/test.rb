bb6a9d23e62ab9c8e495cbcad1da618cc37261e7
katarighe <m.ighe@hotmail.com>
2023-10-03 20:14:59 +0300

    Make expansion to the test

--- classes/test.rb
+++ classes/test.rb
@@ -5,15 +5,18 @@ require_relative 'book'
 
 student1 = Student.new('Unknown', 13, 'John Baker')
 student2 = Student.new('Unknown', 17, 'Mark Adams')
+student3 = Student.new('Unknown', 14, 'George Austin')
 
-class1 = Classroom.new('Literature')
+class1 = Classroom.new('Literature & Travels')
 
 book1 = Book.new('First Class Travellers', 'Author1')
 book2 = Book.new('Flying Home', 'Author2')
+book3 = Book.new('Striking UPS Workers', 'Author3')
 
 Rental.new('2017-12-07', book1, student1)
 Rental.new('2018-03-07', book2, student2)
 Rental.new('2019-08-07', book2, student1)
+Rental.new('2022-02-09', book3, student3)
 
 puts class1.add_student(student1)
 puts student1.classroom.label
@@ -24,8 +27,11 @@ puts class1.students.count
 
 puts book1.rentals
 puts book2.rentals
+puts book3.rentals
 
 puts student1.rentals.count
 puts student2.rentals.count
+puts student3.rentals.count
 puts book1.rentals.count
 puts book2.rentals.count
+puts book3.rentals.count
