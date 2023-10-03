07f63ad0e67db3fad2a9350d06d55f52f1ea3083
katarighe <m.ighe@hotmail.com>
2023-10-03 18:59:19 +0300

    Improve and fix typo

--- classes/classroom.rb
+++ classes/classroom.rb
@@ -1,6 +1,5 @@
 class Classroom
-  attr_accessor :label
-  attr_reader :students
+  attr_accessor :label, :students
 
   def initialize(label)
     @label = label
@@ -9,6 +8,6 @@ class Classroom
 
   def add_student(student)
     @students.push(student)
-    student.classroom(self)
+    student.classroom = self
   end
 end
