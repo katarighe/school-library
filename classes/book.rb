5d8f936e1d781cd8f4727e4079856e44de118071
katarighe <m.ighe@hotmail.com>
2023-10-03 11:05:33 +0300

    Rewrite it again for better functionality

--- classes/book.rb
+++ classes/book.rb
@@ -1,9 +1,13 @@
 class Book
-    attr_reader :rentals
-    attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
-    def initalize(title, author)
-        @title = title
-        @author = author
-        @rental = []
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
 end
