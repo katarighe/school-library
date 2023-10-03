a3099fb5e072dc30b588057d83497c792ab4b097
katarighe <m.ighe@hotmail.com>
2023-10-03 19:54:04 +0300

    Add the test details

--- classes/test.rb
+++ classes/test.rb
@@ -1,19 +1,19 @@
-require_relative './student'
-require_relative './classroom'
-require_relative './rental'
-require_relative './book'
+require_relative 'student'
+require_relative 'classroom'
+require_relative 'rental'
+require_relative 'book'
 
 student1 = Student.new('Unknown', 13, 'Mohamed Aden')
 student2 = Student.new('Unknown', 17, 'Mark Adams')
 
-class314 = Classroom.new('314')
+class1 = Classroom.new('Literature')
 
-class314.add_student(student1)
+class1.add_student(student1)
 student1.classroom.label
 student2.classroom
-class314.students.count
-student2.classroom = class314
-class314.students.count
+class1.students.count
+student2.classroom = class1
+class1.students.count
 
 book1 = Book.new('First Class Travellers', 'Author1')
 book2 = Book.new('Flying Home', 'Author2')
@@ -21,11 +21,11 @@ book2 = Book.new('Flying Home', 'Author2')
 book1.rentals
 book2.rentals
 
-rent1 = Rental.new('2017-12-07', student1, book1)
-rent2 = Rental.new('2019-03-07', student2, book2)
-rent3 = Rental.new('2019-08-07', student1, book2)
+Rental.new('2017-12-07', book1, student1)
+Rental.new('2018-03-07', book2, student2)
+Rental.new('2019-08-07', book2, student1)
 
-student1.rentals.count
-student2.rentals.count
-book1.rentals.count
-book2.rentals.count
+puts student1.rentals.count
+puts student2.rentals.count
+puts book1.rentals.count
+puts book2.rentals.count
