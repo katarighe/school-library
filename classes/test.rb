1609315f943dfb36e1d4f61daaec164ca0ae597f
katarighe <m.ighe@hotmail.com>
2023-10-03 19:13:37 +0300

    Create instances for the associations

--- classes/test.rb
+++ classes/test.rb
@@ -1,12 +1,20 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative './student'
+require_relative './classroom'
+require_relative './person'
+require_relative './rental'
+require_relative './book'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
+class1 = Classroom.new('Mathematics for Beginners')
+student1 = Student.new(12)
+class1.add_student(student1)
+student2 = Student.new(15)
+student2.classroom = class1
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
+book1 = Book.new('book1', 'author1')
+person1 = Person.new(12)
+rental1 = Rental.new('2019-08-07', book1, person1)
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+puts rental1.book.title
+puts rental1.person.age
+puts student2.classroom.label
+puts class1.students.map(&:age)
