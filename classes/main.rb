c348d82057539dbed90d9b28ec9efca1bc908c2b
katarighe <m.ighe@hotmail.com>
2023-10-09 12:37:31 +0300

    Refactor the options code

--- classes/main.rb
+++ classes/main.rb
@@ -1,14 +1,16 @@
 require_relative 'app'
 
 def show_options
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
+  puts <<~OPTIONS
+  Please choose an option by entering a number:
+  1 - List of all books
+  2 - List of all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  7 - Exit
+  OPTIONS
 end
 
 def prompt
