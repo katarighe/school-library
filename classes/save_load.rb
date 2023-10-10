aa31eef7797fdfda7574c8a98df1c44bdb381a77
okari <nyandika15@gmail.com>
2023-10-10 18:27:42 +0300

    Update save_load.rb

--- classes/save_load.rb
+++ classes/save_load.rb
@@ -6,77 +6,94 @@ require 'json'
 
 module SaveLoad
   def save_books
-    File.write('classes/books.json', '')
-    @books.each do |book|
-      json = JSON.generate({ title: book.title, author: book.author })
-      File.write('classes/books.json', "#{json}\n", mode: 'a')
+    File.open('classes/books.json', 'w') do |file|
+      @books.each do |book|
+        json = JSON.generate({ title: book.title, author: book.author })
+        file.puts(json)
+      end
     end
   end
 
   def save_people
-    File.write('classes/people.json', '')
-    @people.each do |person|
-      json = if person.is_a?(Student)
-               JSON.generate({ type: person.class, id: person.id, name: person.name, age: person.age,
-                               parent_permission: person.parent_permission })
-             else
-               JSON.generate({ type: person.class, id: person.id, name: person.name, age: person.age,
-                               specialization: person.specialization })
-             end
-      File.write('classes/people.json', "#{json}\n", mode: 'a')
+    File.open('classes/people.json', 'w') do |file|
+      @people.each do |person|
+        json = if person.is_a?(Student)
+                 JSON.generate({ type: 'Student', id: person.id, name: person.name, age: person.age,
+                                 parent_permission: person.parent_permission })
+               else
+                 JSON.generate({ type: 'Teacher', id: person.id, name: person.name, age: person.age,
+                                 specialization: person.specialization })
+               end
+        file.puts(json)
+      end
     end
   end
 
   def save_rentals
-    File.write('classes/rentals.json', '')
-    @rentals.each do |rental|
-      json = JSON.generate({ date: rental.date, person: @people.index(rental.person), book: @books.index(rental.book) })
-      File.write('classes/rentals.json', "#{json}\n", mode: 'a')
+    File.open('classes/rentals.json', 'w') do |file|
+      @rentals.each do |rental|
+        json = JSON.generate({ date: rental.date, person: @people.index(rental.person), book: @books.index(rental.book) })
+        file.puts(json)
+      end
     end
   end
 
-    def save_data
-      save_books unless File.exist?('classes/books.json')
-      save_people unless File.exist?('classes/people.json')
-      save_rentals unless File.exist?('classes/rentals.json')
-    end
+  def save_data
+    save_books unless File.exist?('classes/books.json')
+    save_people unless File.exist?('classes/people.json')
+    save_rentals unless File.exist?('classes/rentals.json')
+  end
 
-    def load_books
-      return unless File.exist?('classes/books.json')
-    
-      File.foreach('classes/books.json') do |line|
+  def load_books
+    return unless File.exist?('classes/books.json')
+
+    @books = []
+    File.foreach('classes/books.json') do |line|
+      begin
         element = JSON.parse(line)
         new_book = Book.new(element['title'], element['author'])
         @books.push(new_book)
+      rescue JSON::ParserError
+        next
       end
     end
-    
-    def load_people
-      return unless File.exist?('classes/people.json')
-    
-      File.foreach('classes/people.json') do |line|
+  end
+
+  def load_people
+    return unless File.exist?('classes/people.json')
+
+    @people = []
+    File.foreach('classes/people.json') do |line|
+      begin
         element = JSON.parse(line)
         if element['type'] == 'Student'
-          new_student = Student.new(id: element['id'], classroom: 'Unknown', age: element['age'], name: element['name'],
-                                    parent_permission: element['parent_permission'])
-          @people.push(new_student)
+          new_person = Student.new(element['id'], element['name'], element['age'], element['parent_permission'])
         else
-          new_teacher = Teacher.new(id: element['id'], age: element['age'], specialization: element['specialization'],
-                                    name: element['name'])
-          @people.push(new_teacher)
+          new_person = Teacher.new(element['id'], element['name'], element['age'], element['specialization'])
         end
+        @people.push(new_person)
+      rescue JSON::ParserError
+        next
       end
     end
-    
-    def load_rentals
-      return unless File.exist?('classes/rentals.json')
-    
-      File.foreach('classes/rentals.json') do |line|
+  end
+
+  def load_rentals
+    return unless File.exist?('classes/rentals.json')
+
+    @rentals = []
+    File.foreach('classes/rentals.json') do |line|
+      begin
         element = JSON.parse(line)
-        new_rental = Rental.new(element['date'], @people[element['person']], @books[element['book']])
+        rental_person = @people[element['person']]
+        rental_book = @books[element['book']]
+        new_rental = Rental.new(rental_person, rental_book, element['date'])
         @rentals.push(new_rental)
+      rescue JSON::ParserError
+        next
       end
     end
+  end
 
   def load_data
     load_books
