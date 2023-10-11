9bcb9e4689f151c133f388bb36c9e1f7d97946b4
katarighe <m.ighe@hotmail.com>
2023-10-11 19:17:35 +0300

    Add the date and rental inside

--- classes/specs/person_spec.rb
+++ classes/specs/person_spec.rb
@@ -1,6 +1,8 @@
 require 'rspec'
+require 'date'
 require_relative '../book'
 require_relative '../person'
+require_relative '../rental'
 
 describe Person do
   let(:person) { Person.new(18, 'Okari') }
